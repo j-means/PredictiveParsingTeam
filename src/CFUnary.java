be6a9b865d0e15932f9edfdeaa7de8e22aff35f0
Casey Pyburn <casey.pyburn@maine.edu>
2020-11-23 23:56:21 -0500

    Adding Jack's work on CFUnary

--- src/CFUnary.java
+++ src/CFUnary.java
@@ -1,4 +1,7 @@
+
 /*
+
+Coded by: Jack O'Connor
 
 YOU WILL NEED TO CODE THE eval, substitute, AND deepCopy METHODS OF THIS CLASS.
 These are all defined recursively on the CFExp and don't require a lot of code.
@@ -12,83 +15,92 @@ The constructor tests the input so that only valid expressions can be created.
 
 *************************************************************************/
 import java.util.Map;
-public class CFUnary extends CFExp{
-   
+
+public class CFUnary extends CFExp {
+
    private CFExp subExp;
-   
-   public CFUnary(CFExp sb) throws Exception{
-         if (sb == null)
-            throw new Exception("error in CFUnary constructor : subexpression is null");
-         else
-            subExp = sb;
+
+   public CFUnary(CFExp sb) throws Exception {
+      if (sb == null)
+         throw new Exception("error in CFUnary constructor : subexpression is null");
+      else
+         subExp = sb;
    }
-   
-   public String toString(){
+
+   public String toString() {
       return "(" + "-" + subExp.toString() + ")";
    }
-   
+
    /*
-   
-   YOU MUST CODE THIS
-   
-   if env is null 
-      throw an Exception if env with message
-      "error in eval : env is null"
-      
-   else if there is a variable in the expression tree with no entry in
-   env throw an exception with message
-   
-   "error in eval : variable " + s + " is not bound in the environment"
-   
-   where s is the variable name
-   (that will be accomplished in the recursive call)
-   
-   Otherwise, return the value of the expression obtained by using the
-   values given in env and the expression  and the CofinFin complement
-   method.
-   
-   **********************************************************************/
-   
-	public CofinFin eval(Map<String, CofinFin> env) throws Exception{
-      return null;
-      
+    * 
+    * YOU MUST CODE THIS
+    * 
+    * if env is null throw an Exception if env with message
+    * "error in eval : env is null"
+    * 
+    * else if there is a variable in the expression tree with no entry in env throw
+    * an exception with message
+    * 
+    * "error in eval : variable " + s + " is not bound in the environment"
+    * 
+    * where s is the variable name (that will be accomplished in the recursive
+    * call)
+    * 
+    * Otherwise, return the value of the expression obtained by using the values
+    * given in env and the expression and the CofinFin complement method.
+    * 
+    **********************************************************************/
+
+   public CofinFin eval(Map<String, CofinFin> env) throws Exception {
+      if (env == null) {
+         throw new Exception("error in eval : eval is null");
+      }
+      CofinFin newSub = subExp.eval(env);
+      return newSub.complement();
+
    }
-	
+
    /*
-   
-   YOU MUST CODE THIS
-   
-   if bindings is null throw an exception with message
-   
-   "error in substitute : bindings is null"
-   
-   otherwise return a CFUnary object that is constructed from this
-   one and the result of the recursive call on substitute for the
-   subexpression subExp
-   
-   Because the old value does not need to be preserved, it is
-   adequate to replace subExp with its result from substitute,
-   and return this.
-   
-   **********************************************************************/
- 
-	public  CFExp substitute(Map<String, CFExp> bindings)throws Exception{
-      return null;
-      
+    * 
+    * YOU MUST CODE THIS
+    * 
+    * if bindings is null throw an exception with message
+    * 
+    * "error in substitute : bindings is null"
+    * 
+    * otherwise return a CFUnary object that is constructed from this one and the
+    * result of the recursive call on substitute for the subexpression subExp
+    * 
+    * Because the old value does not need to be preserved, it is adequate to
+    * replace subExp with its result from substitute, and return this.
+    * 
+    **********************************************************************/
+
+   public CFExp substitute(Map<String, CFExp> bindings) throws Exception {
+      if (bindings == null) {
+         throw new Exception("error in substitute : bindings is null");
+      }
+      subExp = subExp.substitute(bindings);
+      return this;
+
    }
-   
+
    /*
-   
-   YOU MUST CODE THIS
-   
-   return a new CFUnary object that is a deep copy of this
-   
-   ************************************************************************/
-	public  CFExp deepCopy(){
-      return null;
-      
+    * 
+    * YOU MUST CODE THIS
+    * 
+    * return a new CFUnary object that is a deep copy of this
+    * 
+    ************************************************************************/
+   public CFExp deepCopy() {
+      CFExp newCopy = this.deepCopy();
+      try {
+         CFUnary copy = new CFUnary(newCopy);
+         return copy;
+      } catch (Exception e) {
+         return null;
+      }
+
    }
-	
+
 }
-
-
